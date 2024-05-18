c6c3d4801f8d67f3e25d8b1d253b0110f1f977c2
Lewis Milburn <lewiscmilburn@gmail.com>
2024-05-18 18:35:45 +0100

    Rm backuputil temporarily.

--- src/main/java/net/lewmc/kryptonite/optimiser/Optimiser.java
+++ src/main/java/net/lewmc/kryptonite/optimiser/Optimiser.java
@@ -1,14 +1,12 @@
 package net.lewmc.kryptonite.optimiser;
 
 import net.lewmc.kryptonite.Kryptonite;
-import net.lewmc.kryptonite.utils.BackupUtil;
 import net.lewmc.kryptonite.utils.MessageUtil;
 import org.bukkit.command.CommandSender;
 
 public class Optimiser {
     private final Kryptonite plugin;
     private final MessageUtil message;
-    private final BackupUtil backup;
 
     private enum Software {
         UNKNOWN,
@@ -22,7 +20,6 @@ public class Optimiser {
     public Optimiser(CommandSender cs, Kryptonite plugin) {
         this.plugin = plugin;
         this.message = new MessageUtil(cs);
-        this.backup = new BackupUtil(plugin);
     }
 
     public void runDefault(boolean pregeneratedWorld) {
@@ -30,142 +27,126 @@ public class Optimiser {
 
         this.message.Info("Running Vanilla optimisations");
 
-        if (this.backup.ServerProperties()) {
-            ServerProperties properties = new ServerProperties();
+        ServerProperties properties = new ServerProperties();
 
-            properties.networkCompressionThreshold("256");
-            properties.simulationDistance("4");
-            properties.viewDistance("7");
+        properties.networkCompressionThreshold("256");
+        properties.simulationDistance("4");
+        properties.viewDistance("7");
 
-            properties.save();
-        } else {
-            this.message.Error("Unable to backup Vanilla configuration, aborting...");
-        }
+        properties.save();
 
 
         if (server != Software.UNKNOWN) {
             this.message.Info("Running CraftBukkit optimisations");
-            if (this.backup.Bukkit()) {
-                Bukkit bukkit = new Bukkit(this.plugin);
+            Bukkit bukkit = new Bukkit(this.plugin);
 
-                bukkit.spawnLimits(20, 5, 2, 2, 3, 3, 1);
-                bukkit.ticksPer(10, 400, 400, 400, 400, 400, 400);
+            bukkit.spawnLimits(20, 5, 2, 2, 3, 3, 1);
+            bukkit.ticksPer(10, 400, 400, 400, 400, 400, 400);
 
-                bukkit.save();
-            } else {
-                this.message.Error("Unable to backup Bukkit configuration, aborting...");
-            }
+            bukkit.save();
         } else {
             this.message.Warning("Server not CraftBukkit, skipping...");
         }
 
         if (server != Software.UNKNOWN && server != Software.CRAFTBUKKIT) {
             this.message.Info("Running Spigot optimisations");
-            if (this.backup.Spigot()) {
-                Spigot spigot = new Spigot(this.plugin);
+            Spigot spigot = new Spigot(this.plugin);
 
-                spigot.viewDistance("default");
-                spigot.mobSpawnRange(3);
-                spigot.entityActivationRange(16, 24, 48, 8, 8, 16, 48);
-                spigot.entityTrackingRange(48, 48, 48, 32, 64);
-                spigot.tickInacativeVillagers(false);
-                spigot.nerfSpawnerMobs(true);
-                spigot.mergeRadius(3.5, 4.0);
-                spigot.hopperTransfer(8);
-                spigot.hopperCheck(8);
+            spigot.viewDistance("default");
+            spigot.mobSpawnRange(3);
+            spigot.entityActivationRange(16, 24, 48, 8, 8, 16, 48);
+            spigot.entityTrackingRange(48, 48, 48, 32, 64);
+            spigot.tickInacativeVillagers(false);
+            spigot.nerfSpawnerMobs(true);
+            spigot.mergeRadius(3.5, 4.0);
+            spigot.hopperTransfer(8);
+            spigot.hopperCheck(8);
 
-                spigot.save();
-            } else {
-                this.message.Error("Unable to backup Spigot configuration, aborting...");
-            }
+            spigot.save();
         } else {
             this.message.Warning("Server not Spigot, skipping...");
         }
 
         if (server != Software.UNKNOWN && server != Software.CRAFTBUKKIT && server != Software.SPIGOT) {
             this.message.Info("Running Paper optimisations");
-            if (this.backup.PaperWorld()) {
-                PaperWorld pw = new PaperWorld(this.plugin);
-                pw.delayChunkUnloads(10);
-                pw.maxAutosaveChunksPerTick(8);
-                pw.preventMovingIntoUnloadedChunks(true);
-                pw.entityPerChunkSaveLimit(8, 16, 3, 8, 8, 3, 16, 8, 8, 3, 8, 8, 8, 8, 16, 16, 4);
+            PaperWorld pw = new PaperWorld(this.plugin);
+            pw.delayChunkUnloads(10);
+            pw.maxAutosaveChunksPerTick(8);
+            pw.preventMovingIntoUnloadedChunks(true);
+            pw.entityPerChunkSaveLimit(8, 16, 3, 8, 8, 3, 16, 8, 8, 3, 8, 8, 8, 8, 16, 16, 4);
 
-                pw.ambientDespawnRanges(72, 30);
-                pw.axolotlsDespawnRanges(72, 30);
-                pw.creatureDespawnRanges(72, 30);
-                pw.miscDespawnRanges(72, 30);
-                pw.monsterDespawnRanges(72, 30);
-                pw.undergroundWaterCreatureDespawnRanges(72, 30);
-                pw.waterAmbientDespawnRanges(72, 30);
-                pw.waterCreatureDespawnRanges(72, 30);
+            pw.ambientDespawnRanges(72, 30);
+            pw.axolotlsDespawnRanges(72, 30);
+            pw.creatureDespawnRanges(72, 30);
+            pw.miscDespawnRanges(72, 30);
+            pw.monsterDespawnRanges(72, 30);
+            pw.undergroundWaterCreatureDespawnRanges(72, 30);
+            pw.waterAmbientDespawnRanges(72, 30);
+            pw.waterCreatureDespawnRanges(72, 30);
 
-                pw.perPlayerMobSpawns(true);
-                pw.maxEntityCollisions(2);
-                pw.updatePathfindingOnBlockUpdate(false);
-                pw.fixClimbingBypassingCrammingRule(true);
-                pw.armorStandsTick(false);
-                pw.armorStandsDoCollisionEntityLookups(false);
+            pw.perPlayerMobSpawns(true);
+            pw.maxEntityCollisions(2);
+            pw.updatePathfindingOnBlockUpdate(false);
+            pw.fixClimbingBypassingCrammingRule(true);
+            pw.armorStandsTick(false);
+            pw.armorStandsDoCollisionEntityLookups(false);
 
-                if (server != Software.PUFFERFISH) {
-                    pw.villagerBehaviourTickRates(60, 120);
-                    pw.villagerSensorTickRates(80, 80, 40, 40, 40);
-                } else {
-                    this.message.Info("Running Pufferfish, skipping some steps due to incompatibility...");
-                }
-
-                pw.altItemDespawnRate(
-                        true,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        300,
-                        600
-                );
-
-                pw.redstoneImplementation("ALTERNATE_CURRENT");
-                pw.hopperDisableMoveEvent(false);
-                pw.hopperIgnoreOccludingBlocks(true);
-                pw.tickRatesMobSpawner(2);
-                pw.optimizeExplosions(true);
-
-                if (pregeneratedWorld) {
-                    pw.treasureMapsEnabled(true);
-                } else {
-                    this.message.Warning("Treasure maps disabled, please pregenerate your world to re-enable them.");
-                }
-
-                pw.treasureMapsFindAlreadyDiscovered(true);
-                pw.grassSpreadTickRates(4);
-                pw.containerUpdateTickRates(1);
-                pw.nonPlayerArrowDespawnRate(20);
-                pw.creativeArrowDespawnRate(20);
-
-                pw.save();
+            if (server != Software.PUFFERFISH) {
+                pw.villagerBehaviourTickRates(60, 120);
+                pw.villagerSensorTickRates(80, 80, 40, 40, 40);
             } else {
-                this.message.Error("Unable to backup Paper configuration, aborting...");
+                this.message.Info("Running Pufferfish, skipping some steps due to incompatibility...");
             }
+
+            pw.altItemDespawnRate(
+                    true,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    300,
+                    600
+            );
+
+            pw.redstoneImplementation("ALTERNATE_CURRENT");
+            pw.hopperDisableMoveEvent(false);
+            pw.hopperIgnoreOccludingBlocks(true);
+            pw.tickRatesMobSpawner(2);
+            pw.optimizeExplosions(true);
+
+            if (pregeneratedWorld) {
+                pw.treasureMapsEnabled(true);
+            } else {
+                this.message.Warning("Treasure maps disabled, please pregenerate your world to re-enable them.");
+            }
+
+            pw.treasureMapsFindAlreadyDiscovered(true);
+            pw.grassSpreadTickRates(4);
+            pw.containerUpdateTickRates(1);
+            pw.nonPlayerArrowDespawnRate(20);
+            pw.creativeArrowDespawnRate(20);
+
+            pw.save();
         } else {
             this.message.Warning("Server not Paper, skipping...");
         }
