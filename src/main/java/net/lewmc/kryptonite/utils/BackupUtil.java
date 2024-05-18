47775461151857f0e1040a4e5a237386af421cb5
Lewis Milburn <lewiscmilburn@gmail.com>
2024-05-18 18:07:31 +0100

    Add toggleable backups.

--- src/main/java/net/lewmc/kryptonite/utils/BackupUtil.java
+++ src/main/java/net/lewmc/kryptonite/utils/BackupUtil.java
@@ -6,85 +6,122 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 public class BackupUtil {
     private final LogUtil log;
+    private final Kryptonite plugin;
+    private String backupLocation;
 
     public BackupUtil(Kryptonite plugin) {
         this.log = new LogUtil(plugin);
+        this.plugin = plugin;
 
-        try {
-            Files.createDirectories(Paths.get("kryptonite-backup"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        if (this.plugin.getConfig().getBoolean("backup-config")) {
+
+            OffsetDateTime offsetDateTime = OffsetDateTime.now(ZoneOffset.UTC);
+            DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("uuuu-MM-dd HH-mm-ss");
+
+            this.backupLocation = "plugins/Kryptonite/backups/" + offsetDateTime.format(dateTimeFormat);
+            try {
+                Files.createDirectories(Paths.get(this.backupLocation));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
     public boolean Bukkit() {
-        try {
-            Files.copy(
-                    Paths.get("bukkit.yml"),
-                    Paths.get("kryptonite-backup/bukkit.yml"),
-                    StandardCopyOption.REPLACE_EXISTING
-            );
+        if (this.plugin.getConfig().getBoolean("backup-config")) {
+            try {
+                Files.copy(
+                        Paths.get("bukkit.yml"),
+                        Paths.get(this.backupLocation + "/bukkit.yml"),
+                        StandardCopyOption.REPLACE_EXISTING
+                );
+                return true;
+            } catch (IOException e) {
+                log.severe("Unable to backup bukkit.yml");
+                log.severe(e.toString());
+                return false;
+            }
+        } else {
             return true;
-        } catch (IOException e) {
-            log.severe("Unable to backup bukkit.yml");
-            log.severe(e.toString());
-            return false;
         }
     }
 
     public boolean PaperWorld() {
-        try {
-            Files.copy(
-                    Paths.get("config/paper-world-defaults.yml"),
-                    Paths.get("kryptonite-backup/paper-world-defaults.yml"),
-                    StandardCopyOption.REPLACE_EXISTING
-            );
+        if (this.plugin.getConfig().getBoolean("backup-config")) {
+            try {
+                Files.copy(
+                        Paths.get("config/paper-world-defaults.yml"),
+                        Paths.get(this.backupLocation + "/paper-world-defaults.yml"),
+                        StandardCopyOption.REPLACE_EXISTING
+                );
+                return true;
+            } catch (IOException e) {
+                log.severe("Unable to backup bukkit.yml");
+                log.severe(e.toString());
+                return false;
+            }
+        } else {
             return true;
-        } catch (IOException e) {
-            log.severe("Unable to backup bukkit.yml");
-            log.severe(e.toString());
-            return false;
         }
     }
 
     public boolean ServerProperties() {
-        try {
-            Files.copy(
-                    Paths.get("server.properties"),
-                    Paths.get("kryptonite-backup/server.properties"),
-                    StandardCopyOption.REPLACE_EXISTING
-            );
+        if (this.plugin.getConfig().getBoolean("backup-config")) {
+            try {
+                Files.copy(
+                        Paths.get("server.properties"),
+                        Paths.get(this.backupLocation + "/server.properties"),
+                        StandardCopyOption.REPLACE_EXISTING
+                );
+                return true;
+            } catch (IOException e) {
+                log.severe("Unable to backup server.properties");
+                log.severe(e.toString());
+                return false;
+            }
+        } else {
             return true;
-        } catch (IOException e) {
-            log.severe("Unable to backup server.properties");
-            log.severe(e.toString());
-            return false;
         }
     }
 
     public boolean Spigot() {
-        try {
-            Files.copy(
-                    Paths.get("spigot.yml"),
-                    Paths.get("kryptonite-backup/spigot.yml"),
-                    StandardCopyOption.REPLACE_EXISTING
-            );
+        if (this.plugin.getConfig().getBoolean("backup-config")) {
+            try {
+                Files.copy(
+                        Paths.get("spigot.yml"),
+                        Paths.get(this.backupLocation + "/spigot.yml"),
+                        StandardCopyOption.REPLACE_EXISTING
+                );
+                return true;
+            } catch (IOException e) {
+                log.severe("Unable to backup spigot.yml");
+                log.severe(e.toString());
+                return false;
+            }
+        } else {
             return true;
-        } catch (IOException e) {
-            log.severe("Unable to backup spigot.yml");
-            log.severe(e.toString());
-            return false;
         }
     }
 
     public boolean Purpur() {
-        return false;
+        if (this.plugin.getConfig().getBoolean("backup-config")) {
+            return false;
+        } else {
+            return false;
+        }
     }
 
     public boolean Pufferfish() {
-        return false;
+        if (this.plugin.getConfig().getBoolean("backup-config")) {
+            return false;
+        } else {
+            return false;
+        }
     }
 }
