455560bdf83313b36d4555d63a3e094b3d82093b
Lewis Milburn <lewiscmilburn@gmail.com>
2024-05-26 08:42:23 +0100

    Fix Bukkit patches.

--- src/main/java/net/lewmc/kryptonite/optimiser/Bukkit.java
+++ src/main/java/net/lewmc/kryptonite/optimiser/Bukkit.java
@@ -23,23 +23,23 @@ public class Bukkit {
     }
 
     public void spawnLimits(int monsters, int animals, int waterAnimals, int waterAmbient, int waterUndergroundCreature, int axolotls, int ambient) {
-        this.plugin.getConfig().set("environment.spawn-limits.monsters", monsters);
-        this.plugin.getConfig().set("environment.spawn-limits.animals", animals);
-        this.plugin.getConfig().set("environment.spawn-limits.water-animals", waterAnimals);
-        this.plugin.getConfig().set("environment.spawn-limits.water-ambient", waterAmbient);
-        this.plugin.getConfig().set("environment.spawn-limits.water-underground-creature", waterUndergroundCreature);
-        this.plugin.getConfig().set("environment.spawn-limits.axolotls", axolotls);
-        this.plugin.getConfig().set("environment.spawn-limits.ambient", ambient);
+        this.plugin.getConfig().set("spawn-limits.monsters", monsters);
+        this.plugin.getConfig().set("spawn-limits.animals", animals);
+        this.plugin.getConfig().set("spawn-limits.water-animals", waterAnimals);
+        this.plugin.getConfig().set("spawn-limits.water-ambient", waterAmbient);
+        this.plugin.getConfig().set("spawn-limits.water-underground-creature", waterUndergroundCreature);
+        this.plugin.getConfig().set("spawn-limits.axolotls", axolotls);
+        this.plugin.getConfig().set("spawn-limits.ambient", ambient);
     }
 
     public void ticksPer(int monsters, int animals, int waterAnimals, int waterAmbient, int waterUndergroundCreature, int axolotls, int ambient) {
-        this.plugin.getConfig().set("environment.ticks-per.monster-spawns", monsters);
-        this.plugin.getConfig().set("environment.ticks-per.animal-spawns", animals);
-        this.plugin.getConfig().set("environment.ticks-per.water-spawns", waterAnimals);
-        this.plugin.getConfig().set("environment.ticks-per.water-ambient-spawns", waterAmbient);
-        this.plugin.getConfig().set("environment.ticks-per.water-underground-creature-spawns", waterUndergroundCreature);
-        this.plugin.getConfig().set("environment.ticks-per.axolotl-spawns", axolotls);
-        this.plugin.getConfig().set("environment.ticks-per.ambient-spawns", ambient);
+        this.plugin.getConfig().set("ticks-per.monster-spawns", monsters);
+        this.plugin.getConfig().set("ticks-per.animal-spawns", animals);
+        this.plugin.getConfig().set("ticks-per.water-spawns", waterAnimals);
+        this.plugin.getConfig().set("ticks-per.water-ambient-spawns", waterAmbient);
+        this.plugin.getConfig().set("ticks-per.water-underground-creature-spawns", waterUndergroundCreature);
+        this.plugin.getConfig().set("ticks-per.axolotl-spawns", axolotls);
+        this.plugin.getConfig().set("ticks-per.ambient-spawns", ambient);
     }
 
     public void save() {
