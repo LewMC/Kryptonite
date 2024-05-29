cf7ce8f1152722a0ff0f702b248e825bafdbfd0b
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-05-29 11:12:20 +0100

    Enable Luminol support & change to new GitHub link.

--- src/main/java/net/lewmc/kryptonite/Kryptonite.java
+++ src/main/java/net/lewmc/kryptonite/Kryptonite.java
@@ -31,7 +31,7 @@ public final class Kryptonite extends JavaPlugin {
         this.log.info("█ █ █▀▄  █  █▀▀  █  █▄█ █ ▀█ █  █  ██▄");
         this.log.info("");
         this.log.info("Running Kryptonite version "+this.getDescription().getVersion()+ ".");
-        this.log.info("Please report any issues with Kryptonite to our GitHub repository: https://github.com/lewmilburn/kryptonite/issues");
+        this.log.info("Please report any issues with Kryptonite to our GitHub repository: https://github.com/lewmc/kryptonite/issues");
         this.log.info("");
         this.log.info("Beginning startup...");
         this.log.info("");
@@ -92,6 +92,9 @@ public final class Kryptonite extends JavaPlugin {
         } else if (this.getServer().getName().equals("Folia")) {
             this.server = Software.FOLIA;
             this.log.info("Detected server jar: Folia.");
+        } else if (this.getServer().getName().equals("Luminol")) {
+            this.server = Software.FOLIA;
+            this.log.info("Detected server jar: Luminol.");
         } else if (this.getServer().getName().equals("Purpur")) {
             this.server = Software.PURPUR;
             this.log.info("Detected server jar: Purpur.");
