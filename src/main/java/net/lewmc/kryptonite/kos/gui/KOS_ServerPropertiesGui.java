3b4eb178db2ff834efbaab801afc9541a6651284
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-09-24 21:46:14 +0100

    Add ServerPropertiesGui

--- src/main/java/net/lewmc/kryptonite/kos/gui/KOS_ServerPropertiesGui.java
+++ src/main/java/net/lewmc/kryptonite/kos/gui/KOS_ServerPropertiesGui.java
@@ -1,5 +1,6 @@
 package net.lewmc.kryptonite.kos.gui;
 
+import de.themoep.inventorygui.GuiElement;
 import de.themoep.inventorygui.InventoryGui;
 import de.themoep.inventorygui.StaticGuiElement;
 import net.lewmc.kryptonite.Kryptonite;
@@ -8,6 +9,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 
@@ -98,37 +100,31 @@ public class KOS_ServerPropertiesGui {
             this.gui.addElement(new StaticGuiElement(id,
                     new ItemStack(Material.LIME_CONCRETE),
                     1,
-                    click -> {
-                        click.getGui().close();
-                        return true;
-                    },
+                    click -> this.setInt(ServerProperties.Config.NETWORK_COMPRESSION_THRESHOLD, click, value),
                     ChatColor.DARK_GREEN + "Network Compression Threshold",
                     ChatColor.GREEN + "256",
-                    ChatColor.GREEN + "Ideal value"
+                    ChatColor.GREEN + "Ideal value",
+                    ChatColor.BLUE + "Right click to increase - left click to decrease."
             ));
         } else if (value < 256) {
             this.gui.addElement(new StaticGuiElement(id,
                     new ItemStack(Material.RED_CONCRETE),
                     1,
-                    click -> {
-                        click.getGui().close();
-                        return true;
-                    },
+                    click -> this.setInt(ServerProperties.Config.NETWORK_COMPRESSION_THRESHOLD, click, value),
                     ChatColor.DARK_RED + "Network Compression Threshold",
                     ChatColor.RED + String.valueOf(value),
-                    ChatColor.RED + "Too low."
+                    ChatColor.RED + "Too low.",
+                    ChatColor.BLUE + "Right click to increase - left click to decrease."
             ));
         } else {
             this.gui.addElement(new StaticGuiElement(id,
                     new ItemStack(Material.RED_CONCRETE),
                     1,
-                    click -> {
-                        click.getGui().close();
-                        return true;
-                    },
+                    click -> this.setInt(ServerProperties.Config.NETWORK_COMPRESSION_THRESHOLD, click, value),
                     ChatColor.DARK_RED + "Network Compression Threshold",
                     ChatColor.RED + String.valueOf(value),
-                    ChatColor.RED + "Too high."
+                    ChatColor.RED + "Too high.",
+                    ChatColor.BLUE + "Right click to increase - left click to decrease."
             ));
         }
     }
@@ -140,49 +136,41 @@ public class KOS_ServerPropertiesGui {
             this.gui.addElement(new StaticGuiElement(id,
                     new ItemStack(Material.RED_CONCRETE),
                     1,
-                    click -> {
-                        click.getGui().close();
-                        return true;
-                    },
+                    click -> this.setInt(ServerProperties.Config.VIEW_DISTANCE, click, value),
                     ChatColor.DARK_RED + "View distance",
                     ChatColor.RED + String.valueOf(value),
-                    ChatColor.RED + "Too low - impact to player experience."
+                    ChatColor.RED + "Too low - impact to player experience.",
+                    ChatColor.BLUE + "Right click to increase - left click to decrease."
             ));
         } else if (value > 10) {
             this.gui.addElement(new StaticGuiElement(id,
                     new ItemStack(Material.RED_CONCRETE),
                     1,
-                    click -> {
-                        click.getGui().close();
-                        return true;
-                    },
+                    click -> this.setInt(ServerProperties.Config.VIEW_DISTANCE, click, value),
                     ChatColor.DARK_RED + "View distance",
                     ChatColor.RED + String.valueOf(value),
-                    ChatColor.RED + "Too high - large impact to performance."
+                    ChatColor.RED + "Too high - large impact to performance.",
+                    ChatColor.BLUE + "Right click to increase - left click to decrease."
             ));
         } else if (value > 8) {
             this.gui.addElement(new StaticGuiElement(id,
                     new ItemStack(Material.RED_CONCRETE),
                     1,
-                    click -> {
-                        click.getGui().close();
-                        return true;
-                    },
+                    click -> this.setInt(ServerProperties.Config.VIEW_DISTANCE, click, value),
                     ChatColor.DARK_RED + "View distance",
                     ChatColor.RED + String.valueOf(value),
-                    ChatColor.RED + "High - moderate impact to performance."
+                    ChatColor.RED + "High - moderate impact to performance.",
+                    ChatColor.BLUE + "Right click to increase - left click to decrease."
             ));
         } else {
             this.gui.addElement(new StaticGuiElement(id,
                     new ItemStack(Material.LIME_CONCRETE),
                     1,
-                    click -> {
-                        click.getGui().close();
-                        return true;
-                    },
+                    click -> this.setInt(ServerProperties.Config.VIEW_DISTANCE, click, value),
                     ChatColor.DARK_GREEN + "View distance",
                     ChatColor.GREEN + String.valueOf(value),
-                    ChatColor.GREEN + "Within ideal range."
+                    ChatColor.GREEN + "Within ideal range.",
+                    ChatColor.BLUE + "Right click to increase - left click to decrease."
             ));
         }
     }
@@ -195,37 +183,31 @@ public class KOS_ServerPropertiesGui {
             this.gui.addElement(new StaticGuiElement(id,
                     new ItemStack(Material.RED_CONCRETE),
                     1,
-                    click -> {
-                        click.getGui().close();
-                        return true;
-                    },
+                    click -> this.setInt(ServerProperties.Config.SIMULATION_DISTANCE, click, simuDistance),
                     ChatColor.DARK_RED + "Simulation Distance",
                     ChatColor.RED + String.valueOf(simuDistance),
-                    ChatColor.RED + "Too low - impact to player experience."
+                    ChatColor.RED + "Too low - impact to player experience.",
+                    ChatColor.BLUE + "Right click to increase - left click to decrease."
             ));
         } else if (simuDistance <= viewDistance) {
             this.gui.addElement(new StaticGuiElement(id,
                     new ItemStack(Material.LIME_CONCRETE),
                     1,
-                    click -> {
-                        click.getGui().close();
-                        return true;
-                    },
+                    click -> this.setInt(ServerProperties.Config.SIMULATION_DISTANCE, click, simuDistance),
                     ChatColor.DARK_GREEN + "Simulation Distance",
                     ChatColor.GREEN + String.valueOf(simuDistance),
-                    ChatColor.GREEN + "Lower or equal to view distance."
+                    ChatColor.GREEN + "Lower or equal to view distance.",
+                    ChatColor.BLUE + "Right click to increase - left click to decrease."
             ));
         } else {
             this.gui.addElement(new StaticGuiElement(id,
                     new ItemStack(Material.RED_CONCRETE),
                     1,
-                    click -> {
-                        click.getGui().close();
-                        return true;
-                    },
+                    click -> this.setInt(ServerProperties.Config.SIMULATION_DISTANCE, click, simuDistance),
                     ChatColor.DARK_RED + "Simulation Distance",
                     ChatColor.RED + String.valueOf(simuDistance),
-                    ChatColor.RED + "Higher than view distance - impact to performance."
+                    ChatColor.RED + "Higher than view distance - impact to performance.",
+                    ChatColor.BLUE + "Right click to increase - left click to decrease."
             ));
         }
     }
@@ -239,11 +221,14 @@ public class KOS_ServerPropertiesGui {
                     1,
                     click -> {
                         click.getGui().close();
+                        this.properties.set(ServerProperties.Config.SYNC_CHUNK_WRITES, "true");
+                        this.show();
                         return true;
                     },
                     ChatColor.DARK_RED + "Sync Chunk Writes",
                     ChatColor.RED + "false",
-                    ChatColor.RED + "False - impact to player experience."
+                    ChatColor.RED + "False - impact to player experience.",
+                    ChatColor.BLUE + "Click to toggle true/false"
             ));
         } else {
             this.gui.addElement(new StaticGuiElement(id,
@@ -251,12 +236,26 @@ public class KOS_ServerPropertiesGui {
                     1,
                     click -> {
                         click.getGui().close();
+                        this.properties.set(ServerProperties.Config.SYNC_CHUNK_WRITES, "false");
+                        this.show();
                         return true;
                     },
-                    ChatColor.DARK_GREEN + "Simulation Distance",
+                    ChatColor.DARK_GREEN + "Sync Chunk Writes",
                     ChatColor.GREEN + "true",
-                    ChatColor.GREEN + "Ideal value."
+                    ChatColor.GREEN + "Ideal value.",
+                    ChatColor.BLUE + "Click to toggle true/false"
             ));
         }
+    }
+
+    private boolean setInt(ServerProperties.Config key, GuiElement.Click click, int value) {
+        click.getGui().close();
+        if (click.getType() == ClickType.RIGHT) {
+            this.properties.set(key, String.valueOf(value + 1));
+        } else if (click.getType() == ClickType.LEFT) {
+            this.properties.set(key, String.valueOf(value - 1));
+        }
+        this.show();
+        return true;
     }
 }
