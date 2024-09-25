ceae6287c90ce97fd4e446f8cd86ba5bb9aaef2d
Lewis Milburn <lewiscmilburn@gmail.com>
2024-09-25 10:56:07 +0100

    Add JavaDocs + Object function..

--- src/main/java/net/lewmc/kryptonite/utils/ConfigurationUtil.java
+++ src/main/java/net/lewmc/kryptonite/utils/ConfigurationUtil.java
@@ -7,17 +7,29 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.IOException;
 
+/**
+ * ConfigurationUtil is a helper class for managing YML configuration files.
+ */
 public class ConfigurationUtil {
     private final MessageUtil message;
     private final LogUtil log;
     private YamlConfiguration config;
     private String path;
 
+    /**
+     * Constructor for the ConfigurationUtil class.
+     * @param plugin Kryptonite - Reference to the main plugin class.
+     * @param user CommandSender - The user who sent the command.
+     */
     public ConfigurationUtil(Kryptonite plugin, CommandSender user) {
         this.message = new MessageUtil(user);
         this.log = new LogUtil(plugin);
     }
 
+    /**
+     * Loads a configuration file into memory.
+     * @param path String - The path to the file.
+     */
     public void load(String path) {
         this.config = new YamlConfiguration();
         try {
@@ -29,22 +41,54 @@ public class ConfigurationUtil {
         }
     }
 
+    /**
+     * Sets a key to the requested value.
+     * @param key String - The key.
+     * @param value Object - The value.
+     */
     public void set(String key, Object value) {
         this.config.set(key, value);
     }
 
+    /**
+     * Gets a requested value (could be anything!)
+     * @param key String - The key.
+     * @return Object - The value
+     */
+    public Object get(String key) {
+        return this.config.get(key);
+    }
+
+    /**
+     * Gets a requested value as an integer.
+     * @param key String - The key.
+     * @return int - The value
+     */
     public int getInt(String key) {
         return this.config.getInt(key);
     }
 
+    /**
+     * Gets a requested value as a string.
+     * @param key String - The key.
+     * @return String - The value
+     */
     public String getString(String key) {
         return this.config.getString(key);
     }
 
+    /**
+     * Gets a requested value as a boolean.
+     * @param key String - The key.
+     * @return boolean - The value
+     */
     public boolean getBoolean(String key) {
         return this.config.getBoolean(key);
     }
 
+    /**
+     * Saves the configuration file.
+     */
     public void save() {
         try {
             this.config.save(this.path);
